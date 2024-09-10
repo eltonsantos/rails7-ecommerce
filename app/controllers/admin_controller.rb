c4fabf75558c2f07ff1258623f45c389be34ec41
Elton Santos <eltonaxl@hotmail.com>
2024-09-09 21:23:43 -0300

    Add order and order product

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,6 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    
+    @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
 
   belongs_to :category
   has_many :stocks
+  has_many :order_products
 end
